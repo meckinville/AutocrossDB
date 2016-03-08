115aca942178de12889c3739324cd150a598cf0a
meckinville <meckinville@hotmail.com>
2016-03-08 14:17:25 -0500

    Home Page Updates

--- src/java/autocrossdb/backing/HomePageBean.java
+++ src/java/autocrossdb/backing/HomePageBean.java
@@ -8,16 +8,28 @@ package autocrossdb.backing;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
  * @author rmcconville
  */
 @ManagedBean(name="homePage")
+@ApplicationScoped
 public class HomePageBean 
 {
     private List<String> images;
+    
+    private long eventCount;
+    private long driverCount;
+    private long clubCount;
+    private long runCount;
+    
+    @PersistenceContext
+    private EntityManager em;
     
     @PostConstruct
     public void init()
@@ -27,6 +39,11 @@ public class HomePageBean
         {
             images.add(x + ".jpg");
         }
+        
+        eventCount = (long)em.createQuery("SELECT count(e) from Events e").getResultList().get(0);
+        runCount = (long)em.createQuery("SELECT count(r) from Runs r").getResultList().get(0);
+        driverCount = (long)em.createQuery("SELECT count(distinct r.runDriverName) from Runs r").getResultList().get(0);
+        clubCount = (long)em.createQuery("SELECT count(distinct r.runEventUrl.eventClubName) from Runs r").getResultList().get(0);
     }
 
     public List<String> getImages() {
@@ -36,6 +53,39 @@ public class HomePageBean
     public void setImages(List<String> images) {
         this.images = images;
     }
+
+    public long getEventCount() {
+        return eventCount;
+    }
+
+    public void setEventCount(long eventCount) {
+        this.eventCount = eventCount;
+    }
+
+    public long getDriverCount() {
+        return driverCount;
+    }
+
+    public void setDriverCount(long driverCount) {
+        this.driverCount = driverCount;
+    }
+
+    public long getClubCount() {
+        return clubCount;
+    }
+
+    public void setClubCount(long clubCount) {
+        this.clubCount = clubCount;
+    }
+
+    public long getRunCount() {
+        return runCount;
+    }
+
+    public void setRunCount(long runCount) {
+        this.runCount = runCount;
+    }
+
     
     
 }
